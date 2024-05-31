f4b3e33c419663a134a7acd70f276b39bcd2b535
andersongil <andersongil.dev@gmail.com>
2024-05-30 22:21:47 -0400

    Initial Commit

---
+++ spotyClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spotyClone
+//
+//  Created by Anderson Gil on 30/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spotyClone/spotyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotyCloneApp.swift
+//  spotyClone
+//
+//  Created by Anderson Gil on 30/5/24.
+//
+
+import SwiftUI
+
+@main
+struct spotyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
